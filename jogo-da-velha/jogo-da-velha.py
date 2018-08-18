aed88df51b5d8541ba1dc9a3bf501e9f6c05f65a
Breno Nascimento Faria <brenonf@gmail.com>
2018-08-17 21:19:28 -0300

    Jogo da velha ( Tic-Tac-Toe pt-BR)

--- jogo-da-velha/jogo-da-velha.py
+++ jogo-da-velha/jogo-da-velha.py
@@ -1,15 +1,20 @@
 from random import randint
 
 def escolha():
-    c = 999
+    p = 999
     print('Escolha com qual caractere vai jogar.')
-    while c != 'x' and c!= 'o':
-        c = input("x ou o: ")
-        c = c.lower()
-        if c != 'x' and c != 'o':
+    while p != 'x' and p!= 'o':
+        p = input("x ou o: ")
+        p = p.lower()
+        if p != 'x' and p != 'o':
             print('Os valores "x" ou "o" não foram encontrados, tente de novo!!!')
             print()
-    return c
+    print('Você escolheu {}!'.format(p))
+    if p == 'x':
+        c = 'o'
+    else:
+        c = 'x'
+    return p,c
 
 
 def base_do_jogo():
@@ -51,41 +56,77 @@ def conferir(player):
     #linha
     if a[1] == player and a[2] == player and a[3] == player:
         w = player
+        break
     if a[4] == player and a[5] == player and a[6] == player:
         w = player
+        break
     if a[7] == player and a[8] == player and a[9] == player:
         w = player
+        break
     #coluna
     if a[1] == player and a[4] == player and a[7] == player:
         w = player
+        break
     if a[2] == player and a[5] == player and a[8] == player:
         w = player
+        break
     if a[3] == player and a[6] == player and a[9] == player:
         w = player
+        break
     #diagonais
     if a[1] == player and a[5] == player and a[9] == player:
         w = player
+        break
     if a[3] == player and a[5] == player and a[7] == player:
         w = player
+        break
 
+# cpu em implementação
+def jogada_cpu(cpu):
+    while True:
+        joga = randint(1,9)
+        if a[joga] == 'x' or a[joga] == 'o':
+            print('O campo já foi escolhido')
+            continue
+        a[joga] = cpu
+        cont = 1
+        for i in range(3):
+            print(a[cont] , '|' , a[cont + 1] , '|' , a[cont + 2])
+            cont += 3
+        break
+    print()
+        
+    return a
 
-def cpu():
-    return randint(1,9)
 
     
 
 def start():
-    p = escolha()
+    p,c = escolha()
 
     a = base_do_jogo()
 
+    print("Sorteando o primeiro jogador")
+    primeiro = randint(0,1)
 
-    s
+    if primeiro == 1:
+        print('Você é o primeiro!')
+        while True:
+            jogada(p)
+            conferir
+
+
+
+
+    else:
+        print('O CPU é o primeiro!')
     
-    return p,a
+    
+    
+    return p,a,c
 
 
-p,a = start()
+p,a,c = start()
 
 
 
