ce8c528c1218a4866d312d15aad92149b0760998
Breno Nascimento Faria <brenonf@gmail.com>
2018-08-27 19:35:25 -0300

    Jogo campo minado - minesweeper

--- campo-minado/campo-minado.py
+++ campo-minado/campo-minado.py
@@ -1,3 +1,4 @@
+
 def tabuleiro(n):
     m = list()
     for i in range(n):
@@ -30,4 +31,46 @@ def tabuleiro(n):
     return m
 
 
-tabuleiro(12)
+def impressao(m,cont):
+    n = len(m)
+    cont = n ** 2 + 1
+    for i in range(n):
+        print('|',end='')
+        for j in range(n):
+            if cont <= 9:
+                print('{}'.format(m[i][j]),end='')
+            elif cont >= 10 and cont < 100:
+                print('{}'.format(m[i][j]),end='')
+            else:
+                print('{}'.format(m[i][j]),end='')
+            print('|',end='')
+            cont += 1
+        print(' ',i + 1)
+    for i in range(1,n+1):
+        if i < 9:
+            print(' ',end=' ')
+            print(i,end=' ')
+        elif i == 9:
+            print(' ',end=' ')
+            print(i,end=' ')
+        elif i > 9 and i < 100:
+            print(' ',end=' ')
+            print(i,end='')
+    return m
+
+
+def bomba(m,quant):
+    n = len(m)
+    for i in range(quant):
+        while True:
+            x = ri(0, n-1)
+            y = ri(0, n-1)
+            if m[x][y] != '[ ]':
+                continue
+            else:
+                m[x][y] = '[*]'
+                break
+        
+        
+from random import randint as ri
+m = tabuleiro(7)
