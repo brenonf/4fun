acba37d5528430e78f9c0729fb6c3153ad41032a
Breno Faria <brenonf@gmail.com>
2024-06-05 14:21:09 -0300

    correção final de jogos

--- campo-minado/campo-minado.py
+++ campo-minado/campo-minado.py
@@ -1,79 +1,80 @@
+from copy import deepcopy
+from random import randint as ri
 
 def tabuleiro(n):
-    m = list()
+    """Cria um tabuleiro vazio de tamanho n x n."""
+    m = []
     for i in range(n):
         m.append([])
         for j in range(n):
             m[i].append(' # ')
     cont = 1
     for i in range(n):
-        print('|',end='')
+        print('|', end='')
         for j in range(n):
             if cont <= 9:
-                print('{}'.format(m[i][j]),end='')
+                print('{}'.format(m[i][j]), end='')
             elif cont >= 10 and cont < 100:
-                print('{}'.format(m[i][j]),end='')
+                print('{}'.format(m[i][j]), end='')
             else:
-                print('{}'.format(m[i][j]),end='')
-            print('|',end='')
+                print('{}'.format(m[i][j]), end='')
+            print('|', end='')
             cont += 1
-        print(' ',i + 1)
-    for i in range(1,n+1):
+        print(' ', i + 1)
+    for i in range(1, n + 1):
         if i < 9:
-            print(' ',end=' ')
-            print(i,end=' ')
+            print(' ', end=' ')
+            print(i, end=' ')
         elif i == 9:
-            print(' ',end=' ')
-            print(i,end=' ')
+            print(' ', end=' ')
+            print(i, end=' ')
         elif i > 9 and i < 100:
-            print(' ',end=' ')
-            print(i,end='')
+            print(' ', end=' ')
+            print(i, end='')
     return m
 
-
 def impressao(m):
+    """Imprime o tabuleiro."""
     n = len(m)
     cont = n ** 2 + 1
     for i in range(n):
-        print('|',end='')
+        print('|', end='')
         for j in range(n):
             if cont <= 9:
-                print('{}'.format(m[i][j]),end='')
+                print('{}'.format(m[i][j]), end='')
             elif cont >= 10 and cont < 100:
-                print('{}'.format(m[i][j]),end='')
+                print('{}'.format(m[i][j]), end='')
             else:
-                print('{}'.format(m[i][j]),end='')
-            print('|',end='')
+                print('{}'.format(m[i][j]), end='')
+            print('|', end='')
             cont += 1
-        print(' ',i + 1)
-    for i in range(1,n+1):
+        print(' ', i + 1)
+    for i in range(1, n + 1):
         if i < 9:
-            print(' ',end=' ')
-            print(i,end=' ')
+            print(' ', end=' ')
+            print(i, end=' ')
         elif i == 9:
-            print(' ',end=' ')
-            print(i,end=' ')
+            print(' ', end=' ')
+            print(i, end=' ')
         elif i > 9 and i < 100:
-            print(' ',end=' ')
-            print(i,end='')
+            print(' ', end=' ')
+            print(i, end='')
     print('\n')
 
-
 def espelho(m):
-    mb = list()
-    mb = deepcopy(m)
-    return mb
+    """Cria uma cópia do tabuleiro."""
+    return deepcopy(m)
 
-    
-def bomba(mb,quant):
+def bomba(mb, quant):
+    """Distribui bombas aleatoriamente pelo tabuleiro."""
     n = len(mb)
     for i in range(n):
         for j in range(n):
             mb[j][i] = '   '
     for i in range(quant):
         while True:
-            x = ri(0, n-1)
-            y = ri(0, n-1)
+            x = ri(0, n - 1)
+            y = ri(0, n - 1)
             if mb[x][y] != '   ':
                 continue
             else:
@@ -81,8 +82,11 @@ def bomba(mb,quant):
                 break
     return mb
 
-
-def jogada(m,mb):
+def jogada(m, mb):
+    """Executa a jogada do jogador."""
+    num_bombas = sum(row.count('[B]') for row in mb)  # Conta o número de bombas no tabuleiro
+    celulas_sem_bomba = len(m) * len(m) - num_bombas
+    celulas_reveladas = 0
     while True:
         print('Faça sua jogada')
         l = int(input('Digite a linha: ')) - 1
@@ -91,47 +95,62 @@ def jogada(m,mb):
             if mb[l][c] != '[B]':
                 m[l][c] = mb[l][c]
                 impressao(m)
-            elif mb[l][c] == '[B]':
+                celulas_reveladas += 1
+                if celulas_reveladas == celulas_sem_bomba:
+                    print('Parabéns, você ganhou!')
+                    return False  # Indica que o jogador ganhou o jogo
+            else:
                 print('Você explodiu !!!!')
-                break
+                revelar_bombas(m, mb)
+                impressao(m)
+                return False  # Indica que o jogo deve encerrar
         else:
             print('Essa célula já foi escolhida !')
             continue
-            
+
+
+
+
+
 def numeros_bomba(mb):
+    """Conta o número de bombas ao redor de cada célula."""
     n = len(mb)
     for i in range(n):
         for j in range(n):
-            if mb[i][j] != '[B]'
+            if mb[i][j] != '[B]':
                 cont = 0
-            
+                for a in range(-1, 2):
+                    for b in range(-1, 2):
+                        if 0 <= i + a < n and 0 <= j + b < n:
+                            if mb[i + a][j + b] == '[B]':
+                                cont += 1
+                mb[i][j] = ' {} '.format(cont) if cont != 0 else '   '
+    return mb
+
+def revelar_bombas(m, mb):
+    """Revela todas as bombas no tabuleiro."""
+    n = len(mb)
+    for i in range(n):
+        for j in range(n):
+            if mb[i][j] == '[B]':
+                m[i][j] = '[B]'
+
+def iniciar_jogo():
+    """Inicia o jogo de Campo Minado."""
+    tamanho = int(input("Digite o tamanho do tabuleiro: "))
+    quantidade_bombas = int(input("Digite a quantidade de bombas: "))
     
+    m = tabuleiro(tamanho)
+    mb = espelho(m)
+    mb = bomba(mb, quantidade_bombas)
+    mb = numeros_bomba(mb)
     
-from copy import deepcopy
-from random import randint as ri
-m = tabuleiro(6)
-mb = espelho(m)
-print()
-mb = bomba(mb,12)
-print()
-impressao(mb)
+    while True:
+        if not jogada(m, mb):  # Se a jogada resultar em uma explosão, encerra o jogo
+            break
+        if ' # ' not in [celula for linha in m for celula in linha]:
+            print("Parabéns! Você venceu!")
+            break
 
-
-
-
-
-
-'''
-imprimir assim no futuro:
-     0     1     2     3
-   +-----+-----+-----+-----+
-0  | 11  | -3  |  4  |  8  |
-   +-----+-----+-----+-----+
-1  | -3  | 12  |  6  | 11  |
-   +-----+-----+-----+-----+
-2  |  4  |  6  |  5  | 13  |
-   +-----+-----+-----+-----+
-3  |  8  | 11  | 13  |  5  |
-   +-----+-----+-----+-----+
-
-'''
+if __name__ == "__main__":
+    iniciar_jogo()
--- jogo-da-velha/jogo-da-velha.py
+++ jogo-da-velha/jogo-da-velha.py
@@ -1,9 +1,11 @@
+import os
 from random import randint
+import time
 
 def escolha():
     p = 999
     print('Escolha com qual caractere vai jogar.')
-    while p != 'x' and p!= 'o':
+    while p != 'x' and p != 'o':
         p = input("x ou o: ")
         p = p.lower()
         if p != 'x' and p != 'o':
@@ -14,7 +16,7 @@ def escolha():
         c = 'o'
     else:
         c = 'x'
-    return p,c
+    return p, c
 
 
 def base_do_jogo():
@@ -26,153 +28,203 @@ def base_do_jogo():
     return a
 
 
-def jogada(player,a):
+def jogada(player, a):
     while True:
         print('Faça sua jogada escolhendo as posições disponíveis')
         cont = 1
         for i in range(3):
-            print(a[cont] , '|' , a[cont + 1] , '|' , a[cont + 2])
+            print(a[cont], '|', a[cont + 1], '|', a[cont + 2])
             cont += 3        
         joga = input('Digite um número de 1 a 9: ')
-        if joga != '1' and joga !='2' and joga != '3' and joga != '4' and joga != '5' and joga != '6' and joga != '7' and joga != '8' and joga != '9': 
+        if joga not in {'1', '2', '3', '4', '5', '6', '7', '8', '9'}:
             print()
             continue
-        else:
-            joga = int(joga)
-            if a[joga] == 'x' or a[joga] == 'o':
-                print('O campo já foi escolhido')
-                continue
-            a[joga] = player
-            cont = 1
-            for i in range(3):
-                print(a[cont] , '|' , a[cont + 1] , '|' , a[cont + 2])
-                cont += 3
-            break
-        print()
-        return a
-        
-
-def conferir(player,a):
-    #linha
-    w=0
-    if a[1] == player and a[2] == player and a[3] == player:
-        w = player
-        
-    if a[4] == player and a[5] == player and a[6] == player:
-        w = player
-        
-    if a[7] == player and a[8] == player and a[9] == player:
-        w = player
-        
-    #coluna
-    if a[1] == player and a[4] == player and a[7] == player:
-        w = player
-        
-    if a[2] == player and a[5] == player and a[8] == player:
-        w = player
-        
-    if a[3] == player and a[6] == player and a[9] == player:
-        w = player
-        
-    #diagonais
-    if a[1] == player and a[5] == player and a[9] == player:
-        w = player
-        
-    if a[3] == player and a[5] == player and a[7] == player:
-        w = player
-    
-    
-    if w != 0:
-        return player
-    if w == 0:
-        return 0
-    
-
-# cpu em implementação, será retirada a jogada aleatória para uma mais inteligente.
-def jogada_cpu(cpu,a):
-    print()
-    print('Jogada do CPU')
-    while True:
-        joga = randint(1,9)
-        if a[joga] == 'x' or a[joga] == 'o':
-            #print('O campo já foi escolhido')
+        joga = int(joga)
+        if a[joga] in {'x', 'o'}:
+            print('O campo já foi escolhido')
             continue
-        a[joga] = cpu
+        a[joga] = player
         cont = 1
         for i in range(3):
-            print(a[cont] , '|' , a[cont + 1] , '|' , a[cont + 2])
+            print(a[cont], '|', a[cont + 1], '|', a[cont + 2])
             cont += 3
         break
     print()
+    return a
         
+
+def conferir(player, a):
+    #linha
+    w = 0
+    if (a[1] == player and a[2] == player and a[3] == player or
+        a[4] == player and a[5] == player and a[6] == player or
+        a[7] == player and a[8] == player and a[9] == player or
+        #coluna
+        a[1] == player and a[4] == player and a[7] == player or
+        a[2] == player and a[5] == player and a[8] == player or
+        a[3] == player and a[6] == player and a[9] == player or
+        #diagonais
+        a[1] == player and a[5] == player and a[9] == player or
+        a[3] == player and a[5] == player and a[7] == player):
+        w = player
+
+    return player if w else 0
+
+
+def avaliar(a):
+    #Checar linhas
+    for row in [1, 4, 7]:
+        if a[row] == a[row + 1] == a[row + 2]:
+            if a[row] == 'x':
+                return 10
+            elif a[row] == 'o':
+                return -10
+    
+    #Checar colunas
+    for col in [1, 2, 3]:
+        if a[col] == a[col + 3] == a[col + 6]:
+            if a[col] == 'x':
+                return 10
+            elif a[col] == 'o':
+                return -10
+    
+    #Checar diagonais
+    if a[1] == a[5] == a[9]:
+        if a[1] == 'x':
+            return 10
+        elif a[1] == 'o':
+            return -10
+    if a[3] == a[5] == a[7]:
+        if a[3] == 'x':
+            return 10
+        elif a[3] == 'o':
+            return -10
+    
+    #Se não há vencedor
+    return 0
+
+def minimax(a, depth, is_maximizing):
+    score = avaliar(a)
+    
+    #Se o jogador ganhou
+    if score == 10:
+        return score - depth
+    
+    #Se o CPU ganhou
+    if score == -10:
+        return score + depth
+    
+    #Se não há mais movimentos possíveis (empate)
+    if not any(isinstance(v, int) for v in a.values()):
+        return 0
+    
+    #Se é a vez do jogador
+    if is_maximizing:
+        best = -1000
+        for i in range(1, 10):
+            if isinstance(a[i], int):
+                a[i] = 'x'
+                best = max(best, minimax(a, depth + 1, not is_maximizing))
+                a[i] = i
+        return best
+    #Se é a vez do CPU
+    else:
+        best = 1000
+        for i in range(1, 10):
+            if isinstance(a[i], int):
+                a[i] = 'o'
+                best = min(best, minimax(a, depth + 1, not is_maximizing))
+                a[i] = i
+        return best
+
+def melhor_jogada_cpu(a):
+    melhor_valor = 1000
+    melhor_movimento = -1
+    
+    for i in range(1, 10):
+        if isinstance(a[i], int):
+            a[i] = 'o'
+            movimento_valor = minimax(a, 0, True)
+            a[i] = i
+            if movimento_valor < melhor_valor:
+                melhor_valor = movimento_valor
+                melhor_movimento = i
+    
+    return melhor_movimento
+
+def jogada_cpu(cpu, a):
+    print()
+    print('Jogada do CPU')
+    melhor_movimento = melhor_jogada_cpu(a)
+    a[melhor_movimento] = cpu
+    cont = 1
+    for i in range(3):
+        print(a[cont], '|', a[cont + 1], '|', a[cont + 2])
+        cont += 3
+    print()
     return a
 
 
-    
-
 def start():
-    p,c = escolha()
+    p, c = escolha()
 
     a = base_do_jogo()
 
     print("Sorteando o primeiro jogador:")
-    primeiro = randint(0,1)
+    primeiro = randint(0, 1)
 
     if primeiro == 1:
         print('Você é o primeiro!')
+        time.sleep(4)
         w = 0
         fim = 0
         while w == 0:
-            jogada(p,a)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            jogada(p, a)
             fim += 1
-            w = conferir(p,a)
+            w = conferir(p, a)
             if w != 0 or fim == 9:
                 break
-            jogada_cpu(c,a)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            jogada_cpu(c, a)
             fim += 1
-            w = conferir(c,a)
+            w = conferir(c, a)
             if w != 0 or fim == 9:
                 break
         if p == w:
             print('Você ganhou !!!')
-        if c == w:
+        elif c == w:
             print('Você perdeu !!!')
-        if fim == 9:
+        else:
             print('DEU VELHA!!!!')
-
-
-
 
     else:
         print('O CPU é o primeiro!')
+        time.sleep(4)
         w = 0
         fim = 0
         while w == 0:
-            
-            jogada_cpu(c,a)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            jogada_cpu(c, a)
             fim += 1
-            w = conferir(c,a)
+            w = conferir(c, a)
             if w != 0 or fim == 9:
                 break
-            jogada(p,a)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            jogada(p, a)
             fim += 1
-            w = conferir(p,a)
+            w = conferir(p, a)
             if w != 0 or fim == 9:
                 break
         if p == w:
             print('Você ganhou !!!')
-        if c == w:
+        elif c == w:
             print('Você perdeu !!!')
-        if fim == 9:
+        else:
             print('DEU VELHA!!!!')
     
-    
-    return p,a,c
+    return p, a, c
 
 
-p,a,c=start()
-
-
-
-
-    
+if __name__ == "__main__":
+    p, a, c = start()
